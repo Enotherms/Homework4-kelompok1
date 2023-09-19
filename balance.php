8a78e08396c941c916bfc841488611fabc49cd28
Enotherms <sayabiyyu@gmail.com>
2023-09-19 20:28:30 +0700

    new file:   Bukti/Lihat Transaksi.png 	new file:   Bukti/Tampil Saldo.png 	new file:   Bukti/Transfer Saldo.png 	new file:   Postman testing/Homework4-Postman.postman_collection.json 	renamed:    tampilSaldo.php -> balance.php 	renamed:    database/kelompok1_h4 (1).sql -> database/kelompok1_h4.sql 	new file:   transactions.php

--- balance.php
+++ balance.php
@@ -1,12 +1,12 @@
 <?php
+header("Content-Type: application/json");
 include 'conn.php';
 
-if(isset($_GET['id']) && isset($_GET['password'])) {
-    $id = mysqli_real_escape_string($conn, $_GET['id']);
+if(isset($_GET['account_name']) && isset($_GET['password'])) {
+    $accountName = mysqli_real_escape_string($conn, $_GET['account_name']);
     $password = mysqli_real_escape_string($conn, $_GET['password']);
 
-
-    $query = "SELECT balance FROM customers WHERE id = '$id' AND password = '$password'";
+    $query = "SELECT balance FROM customers WHERE fullname = '$accountName' AND password = '$password'";
     $result = mysqli_query($conn, $query);
 
     if(!$result) {
@@ -19,6 +19,6 @@ if(isset($_GET['id']) && isset($_GET['password'])) {
         echo json_encode(['status' => 'error', 'message' => 'No record found or incorrect password.']);
     }
 } else {
-    echo json_encode(['status' => 'error', 'message' => 'ID and password are required.']);
+    echo json_encode(['status' => 'error', 'message' => 'Account name and password are required.']);
 }
 ?>
